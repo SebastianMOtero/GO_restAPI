61e2821a1986c850fcbaef16b00538efe03a0ca7
SebastianMOtero <34726803+SebastianMOtero@users.noreply.github.com>
2021-05-29 15:03:12 -0300

    test connection added

--- main.go
+++ main.go
@@ -3,27 +3,47 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 
+	db "./db"
 	rds "./rds"
 	server "./server"
 )
 
 const (
-	serverPort = ":8081"
-	timeout    = 10 * time.Second
-	secretName = "connection-string-database"
+	timeout = 10 * time.Second
+)
+
+var (
+	dbHost     = os.Getenv("DB_HOST")
+	dbPort, _  = strconv.Atoi(os.Getenv("DB_PORT"))
+	dbUser     = os.Getenv("DB_USER")
+	dbPassword = os.Getenv("DB_PASSWORD")
+	dbName     = os.Getenv("DB_NAME")
+	serverPort = os.Getenv("SERVER_PORT")
 )
 
 func main() {
-	dbClient := rds.NewRDSClient(secretName)
+	connString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPassword, dbName)
+	dbClient := rds.NewRDSClient(connString)
 
-	s := server.NewServer(dbClient)
+	tasksDB := db.NewTasksDB(dbClient)
+
+	errTestConnection := dbClient.TestConnection()
+	if errTestConnection != nil {
+		fmt.Println("TestConnection", errTestConnection)
+	}
+
+	s := server.NewServer(tasksDB)
 
 	err := startHttpServer(s)
 	if err != nil {
 		fmt.Println("Server startup failed")
 	}
+
+	fmt.Println("Server up")
 }
 
 func startHttpServer(s http.Handler) error {
